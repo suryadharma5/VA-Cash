83f79cced3b629548d796593865df0cd3a006852
suryadharmas <setiawansurya03@gmail.com>
2023-07-28 10:49:15 +0700

    make payment functionality

--- app/src/main/java/ProejctAkhir/myapplication/DetailPage.java
+++ app/src/main/java/ProejctAkhir/myapplication/DetailPage.java
@@ -3,12 +3,70 @@ package ProejctAkhir.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class DetailPage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final int[] num = {0};
+        final int[] price = {10000};
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_page);
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+
+        TextView textNum = findViewById(R.id.stepper_num);
+        TextView total = findViewById(R.id.total);
+        TextView totalprice = findViewById(R.id.totalprice);
+        TextView error = findViewById(R.id.errormsg);
+
+
+        Button button_plus = findViewById(R.id.plus_btn);
+        Button button_min = findViewById(R.id.minus);
+        Button paybutton = findViewById(R.id.paybutton);
+
+        //OnClick Handler
+        button_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               num[0] = num[0] + 1;
+               textNum.setText(String.valueOf(num[0]));
+               total.setText(String.valueOf(num[0]));
+               totalprice.setText("Rp. " + String.valueOf(numberFormat.format(num[0] * price[0])));
+               error.setVisibility(View.GONE);
+            }
+        });
+
+        button_min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if(num[0] > 0){
+                   num[0] -= 1;
+                   textNum.setText(String.valueOf(num[0]));
+                   total.setText(String.valueOf(num[0]));
+                   totalprice.setText("Rp. " + String.valueOf(numberFormat.format(num[0] * price[0])));
+               }else if (num[0] == 0){
+                   error.setVisibility(View.VISIBLE);
+               }
+            }
+        });
+
+        paybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (num[0] == 0){
+                    error.setVisibility(View.VISIBLE);
+                }else {
+                    error.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
     }
 }
